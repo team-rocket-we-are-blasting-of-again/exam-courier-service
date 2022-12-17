585b72779b28e6024e73eb74021a2f50ef6d8b9b
kasarama <cph-mw216@cphbusiness.dk>
2022-12-17 09:35:30 +0100

    add origin *

--- src/main/java/com/teamrocket/control/CourierController.java
+++ src/main/java/com/teamrocket/control/CourierController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("")
 public class CourierController {
