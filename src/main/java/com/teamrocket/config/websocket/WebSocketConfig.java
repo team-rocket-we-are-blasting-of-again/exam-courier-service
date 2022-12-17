09e9f04de139bd9b61cc7ea66416feb8e42d036c
kasarama <cph-mw216@cphbusiness.dk>
2022-12-17 14:12:42 +0100

    origin *, registry websocket allow all

--- src/main/java/com/teamrocket/config/websocket/WebSocketConfig.java
+++ src/main/java/com/teamrocket/config/websocket/WebSocketConfig.java
@@ -14,6 +14,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/app").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/app/delivery").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/app/delivery/*").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
